7a30e09de469626a6c5eb5c9fdec5a142ad1feda
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-04-12 21:54:26 +0200

    index.html

--- pareto.js
+++ pareto.js
@@ -1,27 +1,46 @@
 
-
-gsap.to('.cirkel',{
-
-opacity:1,
-scale:1,
-duration:6,
-onComplete:()=>{
-    gsap.to('.cirkel',{
-    boxShadow: "0 60px 80px rgba(29, 209, 161, 0.6)", 
-    yoyo:true,
-    repeat:-1,
-    y:-25,
-    duration:1.5,
-    ease:"power1.inOut"
+import { gsap } from "gsap";
 
 
-    });
+
+let display = document.querySelector('.display');
+let data = ['A','B','C','D','E','F','G'];
+
+
+function bi(){
+
+   let html = '<ul>';
+
+ for(let i = 0; i < data.length; i++){     
+     html += `<li>${data[i]}</li>`;     
+ }
+
+  html +='</ul>'
+
+  display.innerHTML = html;
+
 }
 
 
+bi();
 
 
-});
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
