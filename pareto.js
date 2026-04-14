18c1e060b354a06096c5a50da6c87ea53b56e868
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-04-14 23:38:51 +0200

    oppdatering

--- pareto.js
+++ pareto.js
@@ -3,26 +3,7 @@ import { gsap } from "gsap";
 
 
 
-let display = document.querySelector('.display');
-let data = ['A','B','C','D','E','F','G'];
 
-
-function bi(){
-
-   let html = '<ul>';
-
- for(let i = 0; i < data.length; i++){     
-     html += `<li>${data[i]}</li>`;     
- }
-
-  html +='</ul>'
-
-  display.innerHTML = html;
-
-}
-
-
-bi();
 
 
  
