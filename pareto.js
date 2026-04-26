6a1965da6c0858a7ecaee8b4404d42c4964ed9b0
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-04-26 20:59:19 +0200

    hei på deg

--- pareto.js
+++ pareto.js
@@ -2,7 +2,33 @@
 import { gsap } from "gsap";
 
 
+const tl = gsap.timeline();
 
+tl
+
+.to('.papi',{
+ opacity:1,
+ scale:1,
+ duration:5,
+ ease:'power2.in'
+})
+
+.to('.papi',{
+ opacity:0,
+ duration:1,
+ ease: 'power2.in'
+
+
+}, '+=1')
+
+
+.to('.nypapi',{
+ opacity:1,
+ scale:1,
+ duration:2,
+ ease: 'power2.inOut'
+
+})
 
 
 
