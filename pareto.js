6fd1d0b029e0341f2f3c7271a954f0d323bde914
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-05-01 12:20:24 +0200

    oknii

--- pareto.js
+++ pareto.js
@@ -2,51 +2,21 @@
 import { gsap } from "gsap";
 
 
-const play = document.querySelector('.btn');
-const pause = document.querySelector('.pbtn');
-const resume = document.querySelector('.rsm')
-const restart = document.querySelector('.restart')
 
 
-const anime = gsap.to('.A',{
+gsap.to('.A',{
 
-opacity:1,
-scale:1,
-duration:5,
-onComplete:()=>{
-   
-     gsap.to('.A',{
-       opacity:0.5,
-       scale:0.5,
-       duration:2,
-       rotateX:1440,
-       ease:'back.in'
-
-     });
-
-}
-
-
-
+ x:680,
+ duration:3,
+ ease:"power2.out"
 
 });
 
-play.addEventListener('click',()=>{
-  anime.play()
-});
-
-pause.addEventListener('click',()=>{
-    anime.pause()
+gsap.to('.B',{
+ y:680,
+ duration:3,
+ ease:"power2.out"
 })
-
-resume.addEventListener('click',()=>{
-   anime.resume()
-});
-
-restart.addEventListener('click',()=>{
-   anime.restart()
-});
-
 
 
 
