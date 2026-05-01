b641c9fc33e862b232ef6e82022acc6a15d97fbc
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-05-01 22:52:35 +0200

    zimetrix

--- pareto.js
+++ pareto.js
@@ -2,25 +2,40 @@
 import { gsap } from "gsap";
 
 
+const bdata = document.querySelector('.btn')
 
-
-gsap.to('.A',{
+const ni = gsap.to('.A',{
 
  x:680,
  duration:3,
  ease:"power2.out"
 
-});
+})
 
-gsap.to('.B',{
+ gsap.to('.B',{
  y:680,
  duration:3,
  ease:"power2.out"
 })
 
+gsap.to('.C',{
+ x:200,
+ duration:5,
+ ease:"power2.out"    
+})
+
+ gsap.to('.D',{
+ y:200,
+ duration:5,
+ ease:"power2.out"   
+})
+
+bdata.addEventListener('click',()=>{
+
+  ni.restart()
 
 
-
+});
 
 
 
