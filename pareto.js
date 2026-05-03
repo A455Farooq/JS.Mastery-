66ff3012f7a78d3b6e9ec6d765a153ae7606f7b9
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-05-03 16:44:56 +0200

    hvitvaskingking

--- pareto.js
+++ pareto.js
@@ -2,41 +2,6 @@
 import { gsap } from "gsap";
 
 
-const bdata = document.querySelector('.btn')
-
-const ni = gsap.to('.A',{
-
- x:680,
- duration:3,
- ease:"power2.out"
-
-})
-
- gsap.to('.B',{
- y:680,
- duration:3,
- ease:"power2.out"
-})
-
-gsap.to('.C',{
- x:200,
- duration:5,
- ease:"power2.out"    
-})
-
- gsap.to('.D',{
- y:200,
- duration:5,
- ease:"power2.out"   
-})
-
-bdata.addEventListener('click',()=>{
-
-  ni.restart()
-
-
-});
-
 
 
 
