e7ce302ea981c4552c4f636c1f432078bff1f2ed
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-05-23 20:38:17 +0200

    bigBoy

--- pareto.js
+++ pareto.js
@@ -4,10 +4,16 @@ import { gsap } from "gsap";
 
 
 gsap.to('.A h1',{
-
     opacity:1,
     ease:'sine.inOut',
     duration:6
-    
-
 })
+
+
+gsap.to('.B img',{
+    opacity:1,
+    scale:1,
+    ease:'sine.inOut',
+    duration:6  
+})
+
