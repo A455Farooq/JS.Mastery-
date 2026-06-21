8f196be2aba490b6c1e2ab3f6878aa1b9200a40b
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-06-21 21:01:28 +0200

    paypal

--- pareto.js
+++ pareto.js
@@ -2,6 +2,18 @@
 import { gsap } from "gsap";
 
 
+confirm('Velkommen til bislett Kebab');
+
+
+let kundevalg = confirm('vanlig kebab = ok \nkylling kebab = cancel');
+
+
+if(kundevalg){
+    confirm('valgt produkt vanlig kebab = pris 100,00 NOK')
+}
+
+
+
 
 
 
