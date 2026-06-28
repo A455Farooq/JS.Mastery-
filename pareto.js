650dce326f4ea6e580afb665349355f871ff4a5f
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-06-28 11:32:46 +0200

    nio

--- pareto.js
+++ pareto.js
@@ -3,32 +3,21 @@ import { gsap } from "gsap";
 
 
 
-
 gsap.to('.A',{
-   x:100,
-   scale:-0.5,
-   ease:'sine.inOut',
-   duration:2 
-})
+    scale:-0.5,
+    duration:2,
+    borderRadius:100,
+    onComplete:()=>{
+    
+        gsap.to('.A',{
+            scale:1.5,
+            duration:3
+        })
+        
 
-gsap.to('.B',{
-  x:200,
-  scale:-0.5,
-  ease:'sine.inOut',
-  duration:3.5,    
-})
 
-gsap.to('.C',{
- x:300,
- y:100,
- scale:-0.5,
- ease:'sine.inOut',
- duration:3.1,onComplete:()=>{
-    gsap.to('.C',{
-      y:300,
-      x:500
-    })
- }
+    }
+
 })
 
  
