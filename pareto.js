282c581093dd689bd32f08676c11f61cc4c115bb
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-07-01 23:27:47 +0200

    niotech

--- pareto.js
+++ pareto.js
@@ -2,10 +2,15 @@
 import { gsap } from "gsap";
 
 
+console.log('brukeren er logget inn');
 
+setTimeout(function(){
 
- 
+ console.log('anti hvit vasking risikostyrings indikator')
 
+},3000);
+
+console.log('data xyz');
 
 
 
