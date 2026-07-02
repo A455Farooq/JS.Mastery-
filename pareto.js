495557c42460a9657a2485a501a864aa889ecd71
A455Farooq <84344585+A455Farooq@users.noreply.github.com>
2026-07-02 23:51:06 +0200

    hac

--- pareto.js
+++ pareto.js
@@ -3,21 +3,7 @@ import { gsap } from "gsap";
 
 
 
-gsap.to('.B',{
-   y:250,
-   duration:2,
-   ease:'sine.inOut'
- 
 
-})
-
-
-
-gsap.to('.C',{
-    x:250,
-    duration:2,
-    ease:'sine.inOut'
-})
 
 
 
